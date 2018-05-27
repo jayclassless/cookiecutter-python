688a6ccb4e21a1fd2e8cedbac5c8a1a3b0e8e45d
Jason Simeone <jay@classless.net>
2018-05-27 09:41:44 -0400

    Wrong name

--- {{ cookiecutter.package_name }}/docs/source/conf.py
+++ {{ cookiecutter.package_name }}/docs/source/conf.py
@@ -129,7 +129,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, '{{ cookiecutter.project_name }}.tex', u'{{ cookiecutter.project_name }} Documentation',
+    (master_doc, '{{ cookiecutter.package_name }}.tex', u'{{ cookiecutter.package_name }} Documentation',
      u'{{ cookiecutter.author_name }}', 'manual'),
 ]
 
@@ -139,7 +139,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, '{{ cookiecutter.project_name }}', u'{{ cookiecutter.project_name }} Documentation',
+    (master_doc, '{{ cookiecutter.package_name }}', u'{{ cookiecutter.package_name }} Documentation',
      [author], 1)
 ]
 
@@ -150,8 +150,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, '{{ cookiecutter.project_name }}', u'{{ cookiecutter.project_name }} Documentation',
-     author, '{{ cookiecutter.project_name }}', 'One line description of project.',
+    (master_doc, '{{ cookiecutter.package_name }}', u'{{ cookiecutter.package_name }} Documentation',
+     author, '{{ cookiecutter.package_name }}', 'One line description of project.',
      'Miscellaneous'),
 ]
 
